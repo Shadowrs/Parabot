4520747f95a9502b90669388dcce5e1c55d17640
Shadowrs <shadow_jak@hotmail.co.uk>
2018-09-10 02:40:18 +0100

    See documentation with this

--- src/main/java/org/parabot/core/asm/ASMClassLoader.java
+++ src/main/java/org/parabot/core/asm/ASMClassLoader.java
@@ -1,7 +1,10 @@
 package org.parabot.core.asm;
 
+import msplit.SplitMethod;
 import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodNode;
 import org.parabot.core.classpath.ClassPath;
 
 import java.net.MalformedURLException;
@@ -12,6 +15,7 @@ import java.security.Permissions;
 import java.security.ProtectionDomain;
 import java.security.cert.Certificate;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -57,7 +61,7 @@ public class ASMClassLoader extends ClassLoader {
         try {
             return getSystemClassLoader().loadClass(name);
         } catch (Exception ignored) {
-
+            System.err.println("Ignored: "+ignored.toString());
         }
         String key = name.replace('.', '/');
         if (classCache.containsKey(key)) {
@@ -65,19 +69,52 @@ public class ASMClassLoader extends ClassLoader {
         }
         ClassNode node = classPath.classes.get(key);
         if (node != null) {
-            classPath.classes.remove(key);
-            Class<?> c = nodeToClass(node);
-            classCache.put(key, c);
-            return c;
+            try {
+                Class<?> c = nodeToClass(node, name.equals("com/jagex/b") ? 0 : ClassWriter.COMPUTE_MAXS); // no flags otherwise it'll be too big. they compiled it in the first place, they must be <1% away from the limit
+                classPath.classes.remove(key);
+                classCache.put(key, c);
+                return c;
+            } catch (RuntimeException e) {
+                System.err.println("[ASM nodeToClass] "+e.toString()+" | target: "+name+" | "+e.getMessage());
+                if (e.getMessage().equals("Method code too large!")) {
+                    // system class loader .. oh wait if i put it on cp in first place then wont even get here
+                    e.printStackTrace();
+                    // TODO using the node, do asm magic and ... remove the old method and replace with the updated & new using that cool repo?
+                    List<MethodNode> methods = node.methods;
+                    MethodNode       big     = null;
+                    for (MethodNode method : methods) {
+                        System.out.println(method.name+" "+method.desc+" "+method.access+" "+method.signature+" "+method.instructions.size()+" "+method.maxStack+" "+method.maxLocals);
+                        if (method.instructions.size() > 30000) {
+                            big = method;
+                            break;
+                        }
+                    }
+                    if (big != null) {
+                        System.out.println("identified big method: "+big+" ... attempting to split");
+                        SplitMethod.Result result = new SplitMethod(Opcodes.ASM5).split(node.name, big, 2000, 15000, 15000);
+                        if (result == null)
+                            throw new NullPointerException("splitmethod failed :(");
+                        node.methods.remove(big);
+                        node.methods.add(result.trimmedMethod);
+                        node.methods.add(result.splitOffMethod);
+
+                        System.out.println("attempting new nodeToClass of customized node!");
+                        Class<?> c = nodeToClass(node, ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES); // splitting removes both, so needed here
+                        classPath.classes.remove(key);
+                        classCache.put(key, c);
+                        return c;
+                    }
+                }
+            }
         }
         return getSystemClassLoader().loadClass(name);
     }
 
-    private final Class<?> nodeToClass(ClassNode node) {
+    private final Class<?> nodeToClass(ClassNode node, int flags) {
         if (super.findLoadedClass(node.name) != null) {
             return findLoadedClass(node.name);
         }
-        ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_MAXS);
+        ClassWriter cw = new ClassWriter(flags);
         node.accept(cw);
         byte[] b = cw.toByteArray();
         return defineClass(node.name.replace('/', '.'), b, 0, b.length,
